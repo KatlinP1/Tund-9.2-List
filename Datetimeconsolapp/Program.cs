0878cfc6e19d5e0f909a0adbd07fe0a40294911a
Opilane TTHK <opilane@tthk.ee>
2019-11-01 15:16:00 +0200

    Add project files.

---
+++ Datetimeconsolapp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Datetimeconsolapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
